b159dcafdddfc289eae3bce154088fa6691e78ad
rupinderpascal <rupinder@pascalailabs.com>
2025-02-18 19:11:07 +0530

    text input changes

--- src/components/common/TextInput/TextInput.tsx
+++ src/components/common/TextInput/TextInput.tsx
@@ -14,7 +14,24 @@ export interface TextInputProps extends Omit<React.InputHTMLAttributes<HTMLInput
   searchInputRef?: React.RefObject<HTMLInputElement>;
 }
 
-const TextInput = ({searchInputRef,value, onChange,boundaryClass, placeholder, className, onClear, iconClass, ...props}: TextInputProps) => {
+const TextInput = ({searchInputRef,value, onChange,boundaryClass, placeholder, className, onClear, iconClass, type, ...props}: TextInputProps) => {
+  const formatValue = (val: string | number | undefined) => {
+    if (type === 'number' && typeof val === 'number') {
+      return val === 0 ? '' : val.toString();
+    }
+    return val;
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (type === 'number') {
+      const numValue = parseFloat(e.target.value);
+      if (numValue < 0) {
+        return;
+      }
+    }
+    onChange?.(e);
+  };
+
   return (
     <div className={cn("", boundaryClass)}>
       {iconClass &&   <div className={cn("", iconClass)}>
@@ -22,9 +39,10 @@ const TextInput = ({searchInputRef,value, onChange,boundaryClass, placeholder, c
       </div>}
       <input
         ref={searchInputRef}
-        type="text"
-        value={value}
-        onChange={onChange}
+        type={type || "text"}
+        value={formatValue(value)}
+        onChange={handleChange}
+        min={type === 'number' ? 0 : undefined}
         placeholder={placeholder}
         className={cn("flex h-[4vh] w-full bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent  rounded-md file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm placeholder:text-gray-500", className)}
         {...props}
